92674510fc34ac02127ab870f6114d7e582a3bd4
Mustafa Uzun <mustafa.uzun@std.izmirekonomi.edu.tr>
2020-06-29 11:35:00 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace netReflectorProtect
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.Title = ".Net Reflector Protect";
+
+            var open = new OpenFileDialog
+            {
+                Multiselect = false,
+                Title = "Open Exe File",
+                Filter = "Exe File|*.exe"
+            };
+
+            using (open)
+            {
+                while (true)
+                {
+                    if (open.ShowDialog() == DialogResult.OK)
+                    {
+
+                        using (FileStream fileStream = new FileStream(open.FileName, FileMode.Open, FileAccess.Write))
+                        {
+                            fileStream.Seek(244L, SeekOrigin.Begin);
+                            fileStream.WriteByte(11);
+                            fileStream.Flush();
+                        }
+
+                        Console.WriteLine(".Net Reflector Protection Successful");
+                        Console.WriteLine("File: " + open.FileName);
+                        Console.WriteLine();
+                        Console.WriteLine("Press enter to make a new one...");
+
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
